82d64c8b61eec6188eca1fcd4ff08a4f600ce9d4
mahnoorraja266 <rmahnoor031@gmail.com>
2025-12-18 14:07:00 +0500

    feat: define descriptive slides dataset for onboarding carousel

---
+++ components/slides.js
@@ -0,0 +1,22 @@
+export default slides = [
+    {
+        key: '1',
+        text: 'Plan your tasks to do, that way you’ll stay organized and you won’t skip any.',
+        image: require('../assets/IconP2.png'),
+      },
+      {
+        key: '2',
+        text: 'Make a full schedule for the whole week and stay organized and productive all days',
+        image: require('../assets/IconP3.png'),
+      },
+      {
+        key: '3',
+        text: 'create a team task, invite people and manage your work together',
+        image: require('../assets/IconP4.png'),
+      },
+      {
+        key: '4',
+        text: 'You informations are secure with us',
+        image: require('../assets/IconP5.png'),
+      },
+]
